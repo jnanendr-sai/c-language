c2d5770d71ba2d87a1277156e3ca23f6ce431f32
Jnanendra Sai <kjsai124@gmail.com>
2025-11-28 19:57:41 +0530

    Add files via upload

---
+++ age.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int age;
+	printf("enter age");
+	scanf("%d",&age);
+	if(age>=18)
+	{
+		printf("the persion is eligible");
+	}
+	else
+	{
+		printf("the persion is not eligible");
+
+	}
+}
